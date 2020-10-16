c2428a630a55f0592c7c762e51675b16c92fc9bf
dmitrijgulacenkov <pingvin1959@yandex.ru>
2020-10-16 13:53:30 +0300

    add user page

---
+++ crypto/UserViewController.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class UserViewController: UIViewController {
+        
+    @IBOutlet weak var label: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        label.text="kek"
+    }
+    
+
+}
+
+
