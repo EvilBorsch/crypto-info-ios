b57baf8963f5b088d069ef674d03290fb36c3494
dmitrijgulacenkov <pingvin1959@yandex.ru>
2020-10-16 12:17:12 +0300

    Initial Commit

---
+++ crypto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  crypto
+//
+//  Created by Дмитрий Гуляченков on 16.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
